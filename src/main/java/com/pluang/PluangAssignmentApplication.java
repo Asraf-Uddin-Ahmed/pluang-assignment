5f51b76c68cd3d00d9ae79846bad9285ff4da9cf
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2022-01-29 12:14:52 +0600

    Initialize Spring boot project

---
+++ src/main/java/com/pluang/PluangAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.pluang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluangAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluangAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluang/PluangAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluangAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
